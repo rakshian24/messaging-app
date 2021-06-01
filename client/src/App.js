c20255f859af81bf717b246235db6590c70034c8
rakshsindhe <35263835+rakshsindhe@users.noreply.github.com>
2021-06-01 19:45:46 +0530

    Added client with react bootstrap & node sass

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import './App.scss';
+
+const App = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Hey Raksh!</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default App;
